1ca4e605439ac465745cf73a9dc8cf70ad521521
kalashnikovqq <outwest2345@gmail.com>
2022-06-11 01:54:46 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace lab6fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(22)))), ((int)(((byte)(21)))));
+            this.button1.Location = new System.Drawing.Point(41, 439);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(376, 76);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y = x * a + b",
+            "y = a * sin(x*b) + c",
+            "y = a * cos(x * b + c)",
+            "y = a * tg(x * b) + c",
+            "y = a * ctg(x * b + c)",
+            "y = b * x^a",
+            "y = a^(x + b)",
+            "y = ((sin(x) + a * x)/((x-(x)^1/2)+1))^b/x"});
+            this.comboBox1.Location = new System.Drawing.Point(100, 207);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 32);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea6.AxisX.InterlacedColor = System.Drawing.Color.Black;
+            chartArea6.AxisX.IsLabelAutoFit = false;
+            chartArea6.BackColor = System.Drawing.Color.Transparent;
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.BackColor = System.Drawing.Color.White;
+            legend6.BorderColor = System.Drawing.Color.Transparent;
+            legend6.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            legend6.ForeColor = System.Drawing.Color.White;
+            legend6.HeaderSeparatorColor = System.Drawing.Color.White;
+            legend6.InterlacedRowsColor = System.Drawing.Color.Transparent;
+            legend6.ItemColumnSeparatorColor = System.Drawing.Color.White;
+            legend6.Name = "Legend1";
+            legend6.TitleForeColor = System.Drawing.Color.White;
+            legend6.TitleSeparatorColor = System.Drawing.Color.White;
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(331, 90);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series11.BackImage = "23; 22; 21";
+            series11.BorderColor = System.Drawing.Color.White;
+            series11.BorderWidth = 3;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series11.Color = System.Drawing.Color.DodgerBlue;
+            series11.LabelBackColor = System.Drawing.Color.Gainsboro;
+            series11.LabelBorderColor = System.Drawing.Color.White;
+            series11.LabelForeColor = System.Drawing.Color.RosyBrown;
+            series11.Legend = "Legend1";
+            series11.MarkerBorderColor = System.Drawing.Color.White;
+            series11.MarkerColor = System.Drawing.Color.White;
+            series11.Name = "Series1";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Legend = "Legend1";
+            series12.Name = "Series2";
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(912, 469);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(96, 297);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(95, 330);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(96, 264);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "x :";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_a.ForeColor = System.Drawing.Color.Black;
+            this.textBox_a.Location = new System.Drawing.Point(131, 292);
+            this.textBox_a.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(56, 29);
+            this.textBox_a.TabIndex = 4;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_b.ForeColor = System.Drawing.Color.Black;
+            this.textBox_b.Location = new System.Drawing.Point(131, 325);
+            this.textBox_b.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(56, 29);
+            this.textBox_b.TabIndex = 5;
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_x.ForeColor = System.Drawing.Color.Black;
+            this.textBox_x.Location = new System.Drawing.Point(131, 259);
+            this.textBox_x.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(56, 29);
+            this.textBox_x.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(95, 363);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "c :";
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_c.ForeColor = System.Drawing.Color.Black;
+            this.textBox_c.Location = new System.Drawing.Point(131, 358);
+            this.textBox_c.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(56, 29);
+            this.textBox_c.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_c);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox_a);
+            this.groupBox1.Controls.Add(this.textBox_x);
+            this.groupBox1.Controls.Add(this.textBox_b);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 594);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(94, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(298, 35);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Построение графиков";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(96, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 35);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Выбор функции:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1245, 687);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Построение графиков";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab6fix
+{
+    public partial class Form1 : Form
+    {
+        double a, b, c, h, l, x, y, x0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_a.Text = "5";
+            textBox_b.Text = "5";
+            textBox_c.Text = "5";
+            textBox_x.Text = "1";
+            chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        //Очистка графика
+        private void ClearGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Проверка ввода
+            if (textBox_a.Text == "" || textBox_b.Text == "" || textBox_x.Text == "")
+            {
+                MessageBox.Show("Введите значения");
+                return;
+            }
+
+            a = Convert.ToInt32(textBox_a.Text);
+            b = Convert.ToInt32(textBox_b.Text);
+            x = Convert.ToInt32(textBox_x.Text);
+            h = 0.1;
+            l = 2;
+            x0 = 1;
+
+            //y = x * a + b
+            if (comboBox1.SelectedIndex == 0)
+            {
+                ClearGraph();
+                y = x0 * a + b;
+                chart1.Series[1].Points.AddXY(x0, y);
+                while (x <= l)
+                {
+                    y = x * a + b;
+                    chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            //y = a * sin(x*b) + c
+            if (comboBox1.SelectedIndex == 1)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = a * Math.Sin(x0 * b) + c;
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+
+
+                        y = a * Math.Sin(x * b) + c;
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = a * cos(x * b + c)
+            if (comboBox1.SelectedIndex == 2)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = a * Math.Cos(x0 * b + c);
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = a * Math.Cos(x * b + c);
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = a * tg(x * b) + c
+            if (comboBox1.SelectedIndex == 3)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = a * Math.Tan(x0 * b) + c;
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = a * Math.Tan(x * b) + c;
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = a * ctg(x * b + c)
+            if (comboBox1.SelectedIndex == 4)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = a * (1 / Math.Tan(x0 * b + c));
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = a * (1 / Math.Tan(x * b + c));
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = b * x^a
+            if (comboBox1.SelectedIndex == 5)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = b * Math.Pow(x0, a);
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = b * Math.Pow(x, a);
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = a^(x + b)
+            if (comboBox1.SelectedIndex == 6)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = Math.Pow(a, (x0 + b));
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = Math.Pow(a, (x + b));
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+            //y = ((sin(x) + a * x)/((x-(x)^1/2)+1))^b/x
+            if (comboBox1.SelectedIndex == 7)
+            {
+                if (textBox_c.Text == "")
+                {
+                    MessageBox.Show("Введите значение C");
+                }
+                else
+                {
+                    c = Convert.ToDouble(textBox_c.Text);
+                    ClearGraph();
+                    y = Math.Pow(((Math.Sin(x0) + a * x0) / ((x0 - Math.Pow((x0), 1 / 2)) + 1)), b / x0);
+                    chart1.Series[1].Points.AddXY(x0, y);
+                    while (x <= l)
+                    {
+                        y = Math.Pow(((Math.Sin(x) + a * x) / ((x - Math.Pow((x), 1 / 2)) + 1)), b / x);
+                        chart1.Series[0].Points.AddXY(x, y);
+                        x += h;
+                    }
+                }
+            }
+        }
+    }
+}
